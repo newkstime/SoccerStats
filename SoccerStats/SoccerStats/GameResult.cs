5fa2f6d43f90349975a743da4fcdf80746a8f78c
Eric Perry <eric.s.perry@outlook.com>
2017-05-11 12:21:59 -0400

    Add files via upload
    App that finds the top 10 MLS players by goals scored from a json file and then searches Bing for articles on those players. Also returns a senitment score from Azure Text Analytics, but at this time the sentiment analyzer only returns a 0.00% score, even in their web based demo.

---
+++ SoccerStats/SoccerStats/GameResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoccerStats
+{
+    public class GameResult
+    {
+        public DateTime GameDate { get; set; }
+        public string TeamName { get; set; }
+        public HomeOrAway HomeOrAway { get; set; }
+        public int Goals { get; set; }
+        public int GoalAttempts { get; set; }
+        public int ShotsOnGoal { get; set; }
+        public int ShotsOffGoal { get; set; }
+        public double PosessionPercent { get; set;}
+        public double ConversionRate
+        {
+            get
+            {
+                return (double)Goals / (double)GoalAttempts;
+            }
+        }
+    }
+
+    public enum HomeOrAway
+    {
+        Home,
+        Away
+    }
+}
